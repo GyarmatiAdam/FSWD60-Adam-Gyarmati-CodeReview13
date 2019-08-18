e88792f32a448003306c8a39204f02d4b3b2aa18
GyarmatiAdam <30920398+GyarmatiAdam@users.noreply.github.com>
2019-08-18 12:44:00 +0200

    new

--- app/config/parameters_production.php
+++ app/config/parameters_production.php
@@ -2,11 +2,11 @@
     $db = parse_url(getenv('POSTGRES_DATABASE_URL'));
 
     $container->setParameter('database_driver', 'pdo_mysql');
-    $container->setParameter('database_host', $db['host']);
-    $container->setParameter('database_port', $db['port']);
-    $container->setParameter('database_name', substr($db["path"], 1));
-    $container->setParameter('database_user', $db['user']);
-    $container->setParameter('database_password', $db['pass']);
+    $container->setParameter('database_host', $db['ec2-54-227-245-146.compute-1.amazonaws.com']);
+    $container->setParameter('database_port', $db['5432']);
+    $container->setParameter('database_name', substr($db["dcckfsh3ruc93r"], 1));
+    $container->setParameter('database_user', $db['cbsorshuvktoss']);
+    $container->setParameter('database_password', $db['0314734fefeb3ff0cb0b04208bda890109a1a2bda9dcaa46799d8195b644df8b']);
     $container->setParameter('secret', getenv('SECRET'));
     $container->setParameter('locale', 'en');
     $container->setParameter('mailer_transport', null);
